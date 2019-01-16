55a8d3a49d320d4640da58975eaee208e62dcec2
Luke Todd <lukejamestodd1@gmail.com>
2019-01-16 12:29:11 +1100

    insta component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Map from './components/map/Map.js';
 import Header from './components/header/Header.js';
+import Insta from './components/insta/Insta.js';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <Map/>
+        <Insta/>
       </div>
     );
   }
---
+++ src/components/insta/Insta.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Insta.css';
+
+class Insta extends Component {
+    render() {
+      return (
+        <div className="Insta">
+        </div>
+      );
+    }
+  }
+  
+  export default Insta;

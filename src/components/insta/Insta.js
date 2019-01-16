2a96b4bd9df2167e85978229b7d601cdacf5c299
Luke Todd <lukejamestodd1@gmail.com>
2019-01-16 16:49:38 +1100

    instagram component

--- src/components/insta/Insta.js
+++ src/components/insta/Insta.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 import './Insta.css';
+import request from 'superagent';
 
 class Insta extends Component {
-    render() {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    }
+  }
+
+  componentWillMount() {
+    this.fetchPhotos();
+  }
+
+  //store token in a .env file instead
+  fetchPhotos() {
+    request
+      .get('https://api.instagram.com/v1/users/self/media/recent/?access_token=6463676.1677ed0.7ecfe8cce8884b168828613f6b221fba')
+      .then((res) => {
+        this.setState({
+          //hard coded my fave 3 photos here
+          photos: [res.body.data[4], res.body.data[6], res.body.data[8]]
+        })
+      })
+  }  
+  
+  render() {
       return (
         <div className="Insta">
-        </div>
+          <div class="photos">
+            {this.state.photos.map((photo, key) => {
+              return (
+                <div class="picture" key={photo.id}>
+                  <img src={photo.images.low_resolution.url} alt={photo.caption}/>
+                </div>
+              )
+            })}
+            <a class="button" href="https://github.com/lukejamestodd1/cornwell" target="_blank"><span>FOLLOW US →</span></a>
+          </div>
+          
+          <div>
+            {console.log(this.state.photos)}
+          </div>
+      </div>
       );
     }
   }
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './Map.css';
+import StyleOptions from './MapStyle.json';
 
 const Location = ({ text }) => <div>
     
@@ -14,172 +15,13 @@ class Map extends Component {
       lat: -37.81,
       lng: 144.936
     },
-    zoom: 14,
+    zoom: 13,
     disableDefaultUI: true,
     zoomControl: false,
     gestureHandling: 'none',
     backgroundColor:"#eeeeee",
     //tried custom styles with Google maps API, not enough time
-    style: [
-      {
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.icon",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "elementType": "labels.text.stroke",
-        "stylers": [
-          {
-            "color": "#f5f5f5"
-          }
-        ]
-      },
-      {
-        "featureType": "administrative.land_parcel",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#bdbdbd"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#ffffff"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#757575"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#dadada"
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#616161"
-          }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#e5e5e5"
-          }
-        ]
-      },
-      {
-        "featureType": "transit.station",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#eeeeee"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-          {
-            "color": "#c9c9c9"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#9e9e9e"
-          }
-        ]
-      }
-    ]
+    style: StyleOptions
   };
     render() {
       return (

70443bd4febcd200dbe2c90f1a15c9dfac9beda6
Luke Todd <lukejamestodd1@gmail.com>
2019-01-16 14:08:32 +1100

    google map added

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,13 +1,205 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './Map.css';
 
+const Location = ({ text }) => <div>
+    
+    <span class="D">{text}</span>
+    <div class="pointer"></div>
+  </div>;
+
 class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: -37.81,
+      lng: 144.936
+    },
+    zoom: 14,
+    disableDefaultUI: true,
+    zoomControl: false,
+    gestureHandling: 'none',
+    backgroundColor:"#eeeeee",
+    //tried custom styles with Google maps API, not enough time
+    style: [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dadada"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c9c9c9"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      }
+    ]
+  };
     render() {
       return (
-        <div className="Map">
-        </div>
+        <div className="Map" style={{ height: '100vh', width: '50%'}}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAJ914nvbWDv457JXNCo8GBPig_F_3HTvw' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          // style={this.props.style}
+        >
+          <Location
+            lat={-37.8102734}
+            lng={144.9365478}
+            text={'D'}
+          />
+        </GoogleMapReact>
+      </div>
       );
     }
   }
   
   export default Map;
+  
